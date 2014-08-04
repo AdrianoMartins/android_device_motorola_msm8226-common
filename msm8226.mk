88a100b1d227656182c76aa9572453d028a73e84
Adriano Martins <adrianomartins@gmail.com>
2014-08-04 03:26:09 -0300

    Enable Root System

--- msm8226.mk
+++ msm8226.mk
@@ -62,7 +62,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.izat.premium_enabled=0 \
     ro.qc.sdk.izat.service_mask=0x0 \
     persist.gps.qc_nlp_in_use=0 \
-    ro.gps.agps_provider=1
+    ro.gps.agps_provider=1 \
+    ro.secure=0
 
 # HAL
 PRODUCT_PACKAGES += \
